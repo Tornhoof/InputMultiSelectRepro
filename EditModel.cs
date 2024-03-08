1722eec29cf1b3d44f39552567d823851ed2d978
Tornhoof <tornhoof@gmail.com>
2024-03-08 10:57:52 +0100

    repro

---
+++ EditModel.cs
@@ -0,0 +1,16 @@
+﻿namespace InputMultiSelectRepro
+{
+    public class EditModel
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsActive { get; set; }
+        public bool IsEmailVerified { get; set; }
+
+        public List<(bool, string)> CurrentRoles { get; set; } = new List<(bool, string)>();
+        public string[] SelectedRoles { get; set; } = [];
+    }
+}
